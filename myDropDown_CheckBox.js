cc1ac96420fa2a89bd1b7b0701a4a42cb20e88c8
Carlos Augusto Oliveira Lopes da Silva <guto.pro@gmail.com>
2013-03-16 20:44:14 -0300

    Alteracoes transformado em classe e em checkbox

--- myDropDown_CheckBox.js
+++ myDropDown_CheckBox.js
@@ -29,22 +29,55 @@ var blueDDLClass = (function(){
 
 	blueDDLClass.generatedList = function(nome, list){
 		var listTxt = '';
+		var checkBox='';
+		if(blueDDLClass.checkBox)
+			checkBox ='<input type=\"checkbox\" />'
 		$(list).each(function(i,o){
-			listTxt = listTxt + '<div class="myDropDownItem" id="{0}" >{1}</div>'.format(nome,$(o).text());
-		});
+				listTxt = listTxt + '<div class="myDropDownItem" id="{0}" >{1} {2}</div>'.format(nome,checkBox,$(o).text());
+			});
 		return listTxt;
 	}
 
+	blueDDLClass.bindEventsDDLCheckBox = function(){
+		if($('#scriptBlueDDL').html().indexOf('myDropDownItemCheckBoxControler') >= 0
+			|| $('#scriptBlueDDLCheckBox').length > 0){
+			return '';
+		}
+		return '<div id=\"scriptBlueDDLCheckBox\"><script type=\"text/javascript\"> \
+            $(function(){  \
+            	$(\'.myDropDown\').each(function(i,o){  \
+                  $(\'input:checkbox\', o).mousedown(blueDDLClass.myDropDownItemCheckBoxControler); \
+                });  \
+           })  \
+           </script></div>'; 
+	}
+
 	blueDDLClass.bindEventsDDL = function(){
-	    return '<div id=\"scriptBlueDDL\"><script type=\"text/javascript\"> \
+	    var retEvent = '<div id=\"scriptBlueDDL\"><script type=\"text/javascript\"> \
             $(function(){  \
                $(\'.myDropDown\').each(function(i,o){  \
                $(\'.myDropDownBox\',o).unbind(\'click\').click(blueDDLClass.myDropDownControler);  \
                $(\'.myDropDownItem\', o).click(blueDDLClass.myDropDownItemControler);  \
+               {0} \
              });  \
            })  \
            </script></div>'; 
+           if(blueDDLClass.checkBox){
+             retEvent = retEvent.format('$(\'input:checkbox\', o).click(blueDDLClass.myDropDownItemCheckBoxControler);');
+           }else{
+             retEvent = retEvent.format('');  
+           }
+           return retEvent;
 	};
+
+	blueDDLClass.myDropDownItemCheckBoxControler = function() {
+		if($(this).is(':checked')){
+			$(this).removeAttr('checked');
+		}else{
+			$(this).attr('checked','checked');
+		}
+		event.stopPropagation()
+	}
 
 	blueDDLClass.myDropDownControler = function() {
 		$('.myDropDownItemContainer', $(this).closest('.myDropDown')).slideToggle("fast");
@@ -52,17 +85,29 @@ var blueDDLClass = (function(){
 	};
 	blueDDLClass.myDropDownItemControler = function() {
 	    $('.myDropDownBoxName', $(this).closest('.myDropDown')).text($(this).text());
-		$('.myDropDownItemContainer', $(this).closest('.myDropDown')).slideToggle("fast");
+
+		
+		if(blueDDLClass.checkBox && event.toElement.type != 'checkbox'){
+			if($('input:checkbox',this).is(':checked')){
+				$('input:checkbox',this).removeAttr('checked');
+			}else{
+				$('input:checkbox',this).attr('checked','checked');
+			}
+		}else{
+			$('.myDropDownItemContainer', $(this).closest('.myDropDown')).slideToggle("fast");
+		}
 		event.stopPropagation();
 
 	    return false;
 	};
 
-	function blueDDLClass(clickDelegate, obj){
+	function blueDDLClass(clickDelegate, obj, checkBox){
 		var nome = $(obj).attr('id');
     	var textGenerated = blueDDLClass.generateDropdown(nome);
    	    var listItem = $('.listItem', obj);
    	    var appendText = '';
+
+   	    blueDDLClass.checkBox = checkBox;
 
    	    //ocultando itens da lista
    	    $('.listItem', obj).hide();
@@ -89,10 +134,18 @@ $('html').click(function() {
  });
 
 $.extend($.fn, {
-    blueDDL: function(itemSelectedClick){
-    	new blueDDLClass(itemSelectedClick, this);
+    blueDDL: function(itemSelectedClick, checkBox){
+    	if(typeof(checkBox) != "undefined")
+    		new blueDDLClass(itemSelectedClick, this, checkBox);
+    	else
+    		new blueDDLClass(itemSelectedClick, this, false);
 
 		if($('#scriptBlueDDL').length == 0)
 			$('body').append(blueDDLClass.bindEventsDDL);
+		//tratativa especial caso já tenha sido adicionado um script e
+		//o primeiro não era checkbox
+		//então precisa vincular o evento checkbox sozinho
+		if(blueDDLClass.checkBox)
+			$('body').append(blueDDLClass.bindEventsDDLCheckBox);
     }
 });
